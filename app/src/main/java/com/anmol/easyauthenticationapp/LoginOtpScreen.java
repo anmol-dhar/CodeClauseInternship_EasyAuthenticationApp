2abc3db58b8f9b37d0aa520dbb8770757f256f8d
Anmol Dhar <anmoldhar4@gmail.com>
2023-11-05 00:51:47 +0530

    Otp and username activity page created.

---
+++ app/src/main/java/com/anmol/easyauthenticationapp/LoginOtpScreen.java
@@ -0,0 +1,14 @@
+package com.anmol.easyauthenticationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginOtpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_otp_screen);
+    }
+}
---
+++ app/src/main/java/com/anmol/easyauthenticationapp/LoginUsernameScreen.java
@@ -0,0 +1,14 @@
+package com.anmol.easyauthenticationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginUsernameScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_username_screen);
+    }
+}
