70683e83556a0c83b268f4c9a59250287be75366
Anmol Dhar <anmoldhar4@gmail.com>
2023-11-06 00:19:54 +0530

    Minor UI changes and OTP verification done using Firebase.

--- app/src/main/java/com/anmol/easyauthenticationapp/LoginPhoneNumberScreen.java
+++ app/src/main/java/com/anmol/easyauthenticationapp/LoginPhoneNumberScreen.java
@@ -2,13 +2,47 @@ package com.anmol.easyauthenticationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.hbb20.CountryCodePicker;
 
 public class LoginPhoneNumberScreen extends AppCompatActivity {
+
+    CountryCodePicker countryCodePicker;
+    EditText phoneInput;
+    Button sendOtpButton;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_phone_number_screen);
+
+        countryCodePicker = findViewById(R.id.loginCountryCode);
+        phoneInput = findViewById(R.id.loginPhoneNumber);
+        sendOtpButton = findViewById(R.id.sendOtpButton);
+        progressBar = findViewById(R.id.loginProgressBar);
+
+        progressBar.setVisibility(View.GONE);
+
+        countryCodePicker.registerCarrierNumberEditText(phoneInput);
+
+        sendOtpButton.setOnClickListener((v)->{
+
+            if(!countryCodePicker.isValidFullNumber()){
+                phoneInput.setError("Phone number not valid");
+                return;
+            }
+            Intent intent = new Intent(LoginPhoneNumberScreen.this, LoginOtpScreen.class);
+            intent.putExtra("phone", countryCodePicker.getFullNumberWithPlus());
+            startActivity(intent);
+
+        });
+
     }
 }
