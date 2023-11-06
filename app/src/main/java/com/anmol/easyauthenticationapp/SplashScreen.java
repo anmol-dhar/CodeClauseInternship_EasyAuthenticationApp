6f790239bae5d3ee4b0ae6f6f335863c3499c729
Anmol Dhar <anmoldhar4@gmail.com>
2023-11-06 12:29:54 +0530

    Username details saved in Firebase and logged in to Main Activity page.

--- app/src/main/java/com/anmol/easyauthenticationapp/SplashScreen.java
+++ app/src/main/java/com/anmol/easyauthenticationapp/SplashScreen.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.anmol.easyauthenticationapp.utils.FirebaseUtil;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
@@ -16,9 +18,15 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, LoginPhoneNumberScreen.class));
+
+                if(FirebaseUtil.isLoggedIn()){
+                    startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                }
+                else {
+                    startActivity(new Intent(SplashScreen.this, LoginPhoneNumberScreen.class));
+                }
                 finish();
             }
-        }, 3000);
+        }, 1000);
     }
 }
